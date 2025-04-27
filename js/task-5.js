5b4c3e39a789b2e3b1338efa223063aa741f0f9d
Mykhailo Pylypiv <developer.mykhailo@gmail.com>
2025-04-27 18:52:24 -0400

    task-5 done

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,17 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btnTochange = document.querySelector(".change-color");
+
+btnTochange.addEventListener("click", changeColor);
+
+function changeColor() {
+  const body = document.querySelector("body");
+  body.style.backgroundColor = getRandomHexColor();
+
+  const bgColor = body.style.backgroundColor;
+
+  const colorOutput = document.querySelector(".color");
+  colorOutput.textContent = bgColor;
+}
